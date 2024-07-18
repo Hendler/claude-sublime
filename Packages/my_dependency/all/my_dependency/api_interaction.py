0ab2c44f2e0577b98554373a0e1d63556164eb78
Ubuntu <ubuntu@ip-10-240-222-222.us-west-2.compute.internal>
2024-07-18 15:48:16 +0000

    Package dependency code and create .sublime-dependency file

---
+++ Packages/my_dependency/all/my_dependency/api_interaction.py
@@ -0,0 +1,9 @@
+class AnthropicAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        # Initialize any other necessary attributes or setup here
+
+    def send_message(self, message):
+        # Implement the logic to send a message to the Anthropic API
+        # This is a placeholder implementation and should be replaced with actual API interaction code
+        pass
